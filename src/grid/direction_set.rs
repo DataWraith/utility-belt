51eb5338a10567128a378399c53338bc6a4c86a1
Johannes Holzfuß <johannes@holzfuss.name>
2023-12-19 20:05:48 +0100

    DirectionSet: add unit tests

--- src/grid/direction_set.rs
+++ src/grid/direction_set.rs
@@ -12,11 +12,65 @@ impl DirectionSet {
         self.0 |= 1 << dir as u8;
     }
 
+    pub fn remove(&mut self, dir: Direction) {
+        self.0 &= !(1 << dir as u8);
+    }
+
     pub fn contains(&self, dir: Direction) -> bool {
         self.0 & (1 << dir as u8) != 0
     }
 
     pub fn iter(&self) -> impl Iterator<Item = Direction> + '_ {
         Direction::all().filter(move |dir| self.contains(*dir))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty() {
+        let set = DirectionSet::empty();
+
+        assert!(!set.contains(Direction::Up));
+        assert!(!set.contains(Direction::Right));
+        assert!(!set.contains(Direction::Down));
+        assert!(!set.contains(Direction::Left));
+    }
+
+    #[test]
+    fn test_insert() {
+        let mut set = DirectionSet::empty();
+
+        set.insert(Direction::Up);
+        set.insert(Direction::Right);
+        set.insert(Direction::Down);
+        set.insert(Direction::Left);
+
+        assert!(set.contains(Direction::Up));
+        assert!(set.contains(Direction::Right));
+        assert!(set.contains(Direction::Down));
+        assert!(set.contains(Direction::Left));
+    }
+
+    #[test]
+    fn test_remove() {
+        let mut set = DirectionSet::empty();
+
+        set.insert(Direction::Up);
+        set.insert(Direction::Right);
+        set.insert(Direction::Down);
+        set.insert(Direction::Left);
+
+        set.remove(Direction::Up);
+        set.remove(Direction::Right);
+        set.remove(Direction::Down);
+        set.remove(Direction::Left);
+
+        assert!(!set.contains(Direction::Up));
+        assert!(!set.contains(Direction::Right));
+        assert!(!set.contains(Direction::Down));
+        assert!(!set.contains(Direction::Left));
     }
 }
