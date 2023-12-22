a4a920d233108e63229aa63eb76ee8f477fbd85c
Johannes Holzfuß <johannes@holzfuss.name>
2023-12-22 17:42:56 +0100

    UnionFind: Don't store indices, make the caller remember them

--- src/math/union_find.rs
+++ src/math/union_find.rs
@@ -13,32 +13,33 @@ use ahash::AHashMap as HashMap;
 ///
 /// See [Wikipedia](https://en.wikipedia.org/wiki/Disjoint-set_data_structure) for more information.
 ///
-// TODO: Do we really want to use a HashMap here? Or should the caller remember the indices?
 #[derive(Default)]
-pub struct UnionFind<T: Hash + Eq> {
-    indices: HashMap<T, usize>,
+pub struct UnionFind {
     parents: Vec<usize>,
     sizes: Vec<usize>,
 }
 
-impl<T: Hash + Eq> UnionFind<T> {
-    pub fn make_set(&mut self, x: T) -> usize {
+impl UnionFind {
+    pub fn make_set(&mut self) -> usize {
         let index = self.parents.len();
 
         self.parents.push(index);
         self.sizes.push(1);
-        self.indices.insert(x, index);
 
         index
     }
 
-    pub fn size_of_set(&mut self, x: &T) -> Option<usize> {
+    pub fn size_of_set(&mut self, x: usize) -> Option<usize> {
         let r = self.find(x)?;
         Some(self.sizes[r])
     }
 
-    pub fn find(&mut self, x: &T) -> Option<usize> {
-        let mut x = *self.indices.get(x)?;
+    pub fn find(&mut self, x: usize) -> Option<usize> {
+        if x >= self.parents.len() {
+            return None;
+        }
+
+        let mut x = x;
 
         while self.parents[x] != x {
             let new_x = self.parents[x];
@@ -54,12 +55,12 @@ impl<T: Hash + Eq> UnionFind<T> {
         Some(x)
     }
 
-    pub fn union(&mut self, x: &T, y: &T) -> Result<(), &str> {
+    pub fn union(&mut self, x: usize, y: usize) -> Result<(), &str> {
         let x_root = self.find(x);
         let y_root = self.find(y);
 
         if x_root.is_none() || y_root.is_none() {
-            return Err("Cannot union elements that are not in the set");
+            return Err("Cannot union elements that are not in the data structure.");
         }
 
         if x_root == y_root {
@@ -88,37 +89,37 @@ mod tests {
     fn union_find_test() {
         let mut uf = UnionFind::default();
 
-        let _a = uf.make_set('a');
-        let _b = uf.make_set('b');
-        let _c = uf.make_set('c');
-        let _d = uf.make_set('d');
-        let _e = uf.make_set('e');
-        let _f = uf.make_set('f');
+        let a = uf.make_set();
+        let b = uf.make_set();
+        let c = uf.make_set();
+        let d = uf.make_set();
+        let e = uf.make_set();
+        let f = uf.make_set();
 
-        let _ = uf.union(&'a', &'b');
-        let _ = uf.union(&'b', &'c');
-        let _ = uf.union(&'d', &'e');
+        let _ = uf.union(a, b);
+        let _ = uf.union(b, c);
+        let _ = uf.union(d, e);
 
-        assert_eq!(uf.find(&'a'), uf.find(&'c'));
-        assert_eq!(uf.find(&'a'), uf.find(&'b'));
-        assert_eq!(uf.find(&'b'), uf.find(&'c'));
+        assert_eq!(uf.find(a), uf.find(c));
+        assert_eq!(uf.find(a), uf.find(b));
+        assert_eq!(uf.find(b), uf.find(c));
 
-        assert_eq!(uf.find(&'d'), uf.find(&'e'));
+        assert_eq!(uf.find(d), uf.find(e));
 
-        assert_ne!(uf.find(&'a'), uf.find(&'d'));
-        assert_ne!(uf.find(&'a'), uf.find(&'e'));
-        assert_ne!(uf.find(&'a'), uf.find(&'f'));
-        assert_ne!(uf.find(&'b'), uf.find(&'d'));
-        assert_ne!(uf.find(&'b'), uf.find(&'e'));
-        assert_ne!(uf.find(&'b'), uf.find(&'f'));
-        assert_ne!(uf.find(&'c'), uf.find(&'d'));
-        assert_ne!(uf.find(&'c'), uf.find(&'e'));
-        assert_ne!(uf.find(&'c'), uf.find(&'f'));
-        assert_ne!(uf.find(&'d'), uf.find(&'f'));
-        assert_ne!(uf.find(&'e'), uf.find(&'f'));
+        assert_ne!(uf.find(a), uf.find(d));
+        assert_ne!(uf.find(a), uf.find(e));
+        assert_ne!(uf.find(a), uf.find(f));
+        assert_ne!(uf.find(b), uf.find(d));
+        assert_ne!(uf.find(b), uf.find(e));
+        assert_ne!(uf.find(b), uf.find(f));
+        assert_ne!(uf.find(c), uf.find(d));
+        assert_ne!(uf.find(c), uf.find(e));
+        assert_ne!(uf.find(c), uf.find(f));
+        assert_ne!(uf.find(d), uf.find(f));
+        assert_ne!(uf.find(e), uf.find(f));
 
-        assert_eq!(uf.size_of_set(&'a'), Some(3));
-        let _ = uf.union(&'a', &'d');
-        assert_eq!(uf.size_of_set(&'e'), Some(5));
+        assert_eq!(uf.size_of_set(a), Some(3));
+        let _ = uf.union(a, d);
+        assert_eq!(uf.size_of_set(e), Some(5));
     }
 }
