ea61a2b4339c2b5c4d93a9a3b11c766a08c2f655
Johannes Holzfuß <johannes@holzfuss.name>
2023-12-29 21:42:37 +0100

    math: functions for fitting quadratic/cubic polynomials to data sequences without noise

--- src/math/polynomials/mod.rs
+++ src/math/polynomials/mod.rs
@@ -7,10 +7,10 @@ pub fn differences<T: Num + Signed + Clone>(xs: &[T]) -> Vec<T> {
 
 /// Determines the degree of the polynomial that generates the given sequence.
 /// Assumes that the sequence does not contain noise.
-pub fn polynomial_degree<T: Num + Signed + Clone>(xs: &[T]) -> usize {
-    let mut cur = differences(xs);
+pub fn polynomial_degree<T: Num + Signed + Clone>(ys: &[T]) -> usize {
+    let mut cur = differences(ys);
 
-    for degree in 0..(xs.len() - 1) {
+    for degree in 0..(ys.len() - 1) {
         if cur.iter().all(|x| *x == T::zero()) {
             return degree;
         }
@@ -18,7 +18,64 @@ pub fn polynomial_degree<T: Num + Signed + Clone>(xs: &[T]) -> usize {
         cur = differences(&cur);
     }
 
-    xs.len() - 1
+    ys.len() - 1
+}
+
+/// Determines the coefficients of a quadratic polynomial from data. Assumes
+/// that there is no noise in the data. Assumes that x is 1, 2, 3, ... and the
+/// corresponding y is given in the `ys` argument.
+///
+/// The return type is ordered from highest degree to lowest degree, so that the
+/// returned coefficients (a, b, c) correspond to the polynomial `ax² + bx + c`.
+///
+/// Reference: https://www.radfordmathematics.com/algebra/sequences-series/difference-method-sequences/quadratic-sequences.html
+pub fn fit_quadratic<T: Num + Signed + Clone>(ys: &[T]) -> Option<(T, T, T)> {
+    let first_diffs = differences(ys);
+    let second_diffs = differences(&first_diffs);
+    let third_diffs = differences(&second_diffs);
+
+    if third_diffs.is_empty() || third_diffs.iter().any(|d| *d != T::zero()) {
+        return None;
+    }
+
+    let a = second_diffs[0].clone() / (T::one() + T::one());
+    let b = first_diffs[0].clone() - a.clone() - a.clone() - a.clone();
+    let c = ys[0].clone() - a.clone() - b.clone();
+
+    Some((a, b, c))
+}
+
+/// Determines the coefficients of a cubic polynomial from data. Assumes
+/// that there is no noise in the data. Assumes that x is 1, 2, 3, ... and the
+/// corresponding y is given in the `ys` argument.
+///
+/// The return type is ordered from highest degree to lowest degree, so that the
+/// returned coefficients (a, b, c, d) correspond to the polynomial `ax³ + bx² +
+/// cx + d.`
+///
+/// Reference: https://www.radfordmathematics.com/algebra/sequences-series/difference-method-sequences/cubic-sequences.html
+pub fn fit_cubic<T: Num + Signed + Clone>(ys: &[T]) -> Option<(T, T, T, T)> {
+    let first_diffs = differences(ys);
+    let second_diffs = differences(&first_diffs);
+    let third_diffs = differences(&second_diffs);
+    let fourth_diffs = differences(&third_diffs);
+
+    if fourth_diffs.is_empty() || fourth_diffs.iter().any(|d| *d != T::zero()) {
+        return None;
+    }
+
+    let two = T::one() + T::one();
+    let three = two.clone() + T::one();
+    let six = three.clone() + three.clone();
+    let twelve = six.clone() + six.clone();
+
+    let a = third_diffs[0].clone() / six.clone();
+    let b = (second_diffs[0].clone() - twelve.clone() * a.clone()) / two.clone();
+    let c =
+        first_diffs[0].clone() - three.clone() * b.clone() - (six.clone() + T::one()) * a.clone();
+    let d = ys[0].clone() - a.clone() - b.clone() - c.clone();
+
+    Some((a, b, c, d))
 }
 
 #[cfg(test)]
@@ -51,9 +108,9 @@ mod tests {
 
     #[test]
     fn test_differences() {
-        let xs: Vec<isize> = quadratic_sequence().take(10).collect();
+        let ys: Vec<isize> = quadratic_sequence().take(10).collect();
 
-        let fd = differences(&xs);
+        let fd = differences(&ys);
         let sd = differences(&fd);
         let td = differences(&sd);
 
@@ -62,13 +119,41 @@ mod tests {
 
     #[test]
     fn test_polynomial_degree() {
-        let xs: Vec<i8> = constant_sequence().take(10).collect();
-        assert_eq!(polynomial_degree(&xs), 0);
+        let ys: Vec<i8> = constant_sequence().take(10).collect();
+        assert_eq!(polynomial_degree(&ys), 0);
 
-        let xs: Vec<i32> = linear_sequence().take(10).collect();
-        assert_eq!(polynomial_degree(&xs), 1);
+        let ys: Vec<i32> = linear_sequence().take(10).collect();
+        assert_eq!(polynomial_degree(&ys), 1);
 
-        let xs: Vec<isize> = quadratic_sequence().take(10).collect();
-        assert_eq!(polynomial_degree(&xs), 2);
+        let ys: Vec<isize> = quadratic_sequence().take(10).collect();
+        assert_eq!(polynomial_degree(&ys), 2);
+    }
+
+    #[test]
+    fn test_fit_quadratic() {
+        let ys = vec![3699, 91951, 297707, 620967];
+        let (a, b, c) = fit_quadratic(&ys).unwrap();
+        let f = |x: isize| a * x * x + b * x + c;
+
+        assert_eq!(f(1), ys[0]);
+        assert_eq!(f(2), ys[1]);
+        assert_eq!(f(3), ys[2]);
+        assert_eq!(f(4), ys[3]);
+    }
+
+    #[test]
+    fn test_fit_cubic() {
+        let ys = vec![4, 14, 40, 88, 164];
+        let (a, b, c, d) = fit_cubic(&ys).unwrap();
+
+        assert_eq!(a, 1);
+        assert_eq!(b, 2);
+        assert_eq!(c, -3);
+        assert_eq!(d, 4);
+
+        let ys = quadratic_sequence().take(10).collect::<Vec<_>>();
+        let (a, b, c, d) = fit_cubic(&ys).unwrap();
+
+        assert_eq!(a, 0);
     }
 }
