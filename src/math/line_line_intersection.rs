a2dd49ef21f46c52e1c2a68c5921af9e2a299561
Johannes Holzfuß <johannes@holzfuss.name>
2024-01-01 00:17:34 +0100

    math: Use the Wikipedia formula for Line intersection
    The downside of this is that it may overflow during multiplication. I'll have
    to look for a different formula that's more numerically stable eventually.

--- src/math/line_line_intersection.rs
+++ src/math/line_line_intersection.rs
@@ -2,48 +2,49 @@ use num::{Num, Signed};
 
 // This computes the intersection point of two lines (if any), given as pairs of points.
 //
-// TODO: Handle the case where the lines are vertical or identical.
-pub fn line_intersection_point<T: Num + Copy + Signed>(
+// https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+//
+// NOTE: This returns `None` for coincidental lines, you'll have to check for
+//       that separately.
+//
+// NOTE: If you don't use sufficiently large numeric types, this may overflow
+//       during multiplication and panic.
+pub fn line_intersection_point<T: Num + Clone + Signed>(
     a: ((T, T), (T, T)),
     b: ((T, T), (T, T)),
 ) -> Option<(T, T)> {
-    let x1 = (a.0).0;
-    let y1 = (a.0).1;
-    let dx1 = (a.1).0 - x1;
-    let dy1 = (a.1).1 - y1;
+    let x1 = &(a.0).0;
+    let y1 = &(a.0).1;
+    let x2 = &(a.1).0;
+    let y2 = &(a.1).1;
 
-    let x2 = (b.0).0;
-    let y2 = (b.0).1;
-    let dx2 = (b.1).0 - x2;
-    let dy2 = (b.1).1 - y2;
+    let x3 = &(b.0).0;
+    let y3 = &(b.0).1;
+    let x4 = &(b.1).0;
+    let y4 = &(b.1).1;
 
-    // If both lines are vertical then they are parallel and will never intersect.
-    if dx1 == T::zero() && dx2 == T::zero() {
+    let denominator = (x1.clone() - x2.clone()) * (y3.clone() - y4.clone())
+        - (y1.clone() - y2.clone()) * (x3.clone() - x4.clone());
+
+    if denominator.is_zero() {
         return None;
     }
 
-    // TODO: Handle this case
-    if dx1 == T::zero() || dx2 == T::zero() {
-        return None;
-    }
+    let x = (x1.clone() * y2.clone() - y1.clone() * x2.clone()) * (x3.clone() - x4.clone())
+        - (x1.clone() - x2.clone()) * (x3.clone() * y4.clone() - y3.clone() * x4.clone())
+            / denominator.clone();
 
-    // Calculate slopes
-    let m1 = dy1 / dx1;
-    let m2 = dy2 / dx2;
-
-    // Identical slopes -- The lines are either identical or parallel.
-    if m1 == m2 {
-        return None;
-    }
-
-    let x = (m1 * x1 - m2 * x2 + y2 - y1) / (m1 - m2);
-    let y = m1 * (x - x1) + y1;
+    let y = (x1.clone() * y2.clone() - y1.clone() * x2.clone()) * (y3.clone() - y4.clone())
+        - (y1.clone() - y2.clone()) * (x3.clone() * y4.clone() - y3.clone() * x4.clone())
+            / denominator.clone();
 
     Some((x, y))
 }
 
 #[cfg(test)]
 mod tests {
+    use num::rational::Ratio;
+
     use super::*;
 
     #[test]
@@ -62,5 +63,26 @@ mod tests {
             line_intersection_point(((0, 0), (1, 1)), ((2, 2), (3, 3))),
             None
         )
+    }
+
+    #[test]
+    fn test_with_rationals() {
+        let x1 = Ratio::new(0xfbbb8c2bc3ed57i128, 1);
+        let y1 = Ratio::new(0xb120ca41205a7ci128, 1);
+
+        let x2 = Ratio::new(0x649fefb18d3ed0i128, 1);
+        let y2 = Ratio::new(0xfd63e6bbb2f6a9i128, 1);
+
+        let dx1 = Ratio::new(1i128, 1i128);
+        let dy1 = Ratio::new(1i128, 1i128);
+
+        let dx2 = Ratio::new(1i128, 1i128);
+        let dy2 = Ratio::new(-1i128, 1i128);
+
+        assert!(dbg!(line_intersection_point(
+            ((x1, y1), (x1 + dx1, y1 + dy1)),
+            ((x2, y2), (x2 + dx2, y2 + dy2))
+        ))
+        .is_some());
     }
 }
