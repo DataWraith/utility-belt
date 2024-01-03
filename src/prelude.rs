4f052cb3502ef5431f4c5be5f9c8d8036dd31072
Johannes Holzfuß <johannes@holzfuss.name>
2024-01-03 12:41:39 +0100

    Try and fix comemo

--- src/prelude.rs
+++ src/prelude.rs
@@ -3,7 +3,8 @@ pub use ahash::AHashMap as HashMap;
 pub use ahash::AHashSet as HashSet;
 
 // comemo
-pub use comemo::memoize;
+pub use comemo;
+pub use comemo::*;
 
 // glam
 pub use glam::I64Vec2;
