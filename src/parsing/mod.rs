eca76ed36868e35fcd847500e460c1205bd23c49
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-01 07:25:53 +0100

    Fix parse_ints function

--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -1,6 +1,7 @@
-use winnow::{
+pub use winnow::{
     ascii::digit1,
-    combinator::{opt, preceded, separated},
+    combinator::{opt, preceded, repeat, separated},
+    token::{none_of, take_till},
     PResult, Parser,
 };
 
@@ -14,9 +15,16 @@ fn parse_int(input: &mut &str) -> PResult<i64> {
 }
 
 pub fn parse_ints(input: &str) -> Vec<i64> {
-    separated(1.., parse_int, any)
-        .parse(input)
-        .expect("Couldn't parse ints")
+    separated(
+        1..,
+        parse_int,
+        (
+            any,
+            take_till(0.., |c: char| c.is_ascii_digit() || c == '-'),
+        ),
+    )
+    .parse(input)
+    .expect("Couldn't parse ints")
 }
 
 #[cfg(test)]
@@ -26,10 +34,11 @@ mod tests {
     #[test]
     fn test_parse_ints() {
         assert_eq!(parse_ints("123"), vec![123]);
-        assert_eq!(parse_ints("123-456"), vec![123, 456]);
+        assert_eq!(parse_ints("123 456"), vec![123, 456]);
         assert_eq!(parse_ints("123 456.789"), vec![123, 456, 789]);
         assert_eq!(parse_ints("123 456 -789"), vec![123, 456, -789]);
         assert_eq!(parse_ints("-123@456,-789"), vec![-123, 456, -789]);
         assert_eq!(parse_ints("123--456--789"), vec![123, -456, -789]);
+        assert_eq!(parse_ints("123   456\n789"), vec![123, 456, 789]);
     }
 }
