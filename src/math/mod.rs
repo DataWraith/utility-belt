575d68f90ced2db90512902aa9a64d65c2de746c
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-15 11:49:08 +0100

    Remove code that is not likely to be useful
    - MADGRAD
    - Breadth-First Search
    - Depth-First Search
    - Rectangle Search
    
    - MADGRAD was never useful, it was just fun to implement.
    - Breadth-First and Depth-First Search might be useful, but it's faster to 
      just re-implement them because they are so simple.
    - Rectangle Search was experimental, and similar to MADGRAD, just for fun.

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,7 +1,6 @@
 pub mod cumsum;
 pub mod gauss_jordan;
 pub mod line_line_intersection;
-pub mod madgrad;
 pub mod modular;
 pub mod polygons;
 pub mod polynomials;
@@ -9,7 +8,6 @@ pub mod polynomials;
 pub use cumsum::*;
 pub use gauss_jordan::*;
 pub use line_line_intersection::*;
-pub use madgrad::*;
 pub use modular::*;
 pub use polygons::*;
 pub use polynomials::*;
--- src/search/mod.rs
+++ src/search/mod.rs
@@ -2,14 +2,8 @@ mod astar;
 mod beam;
 mod bisect;
 mod brents;
-mod brfs;
-mod dfs;
-mod rectangle;
 
 pub use astar::*;
 pub use beam::*;
 pub use bisect::*;
 pub use brents::*;
-pub use brfs::*;
-pub use dfs::*;
-pub use rectangle::*;
