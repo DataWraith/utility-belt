08e0abd3b4d284c24df1e911f881befe607e0714
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-15 14:14:18 +0100

    Implement Chinese Remainder Theorem

--- src/math/modular.rs
+++ src/math/modular.rs
@@ -1,8 +1,61 @@
+use num::integer::gcd;
+use num::{Integer, Unsigned};
+
 pub use num_modular::{ModularCoreOps, ModularInteger, ModularPow, ModularUnaryOps};
+
+pub struct Congruence<T: Integer + Unsigned> {
+    a: T,
+    m: T,
+}
+
+pub fn chinese_remainder_theorem<
+    'a,
+    T: Clone + Integer + Unsigned + ModularUnaryOps<&'a T, Output = T>,
+>(
+    congruences: &'a [Congruence<T>],
+) -> Option<T> {
+    let mut n = T::one();
+
+    for congruence in congruences.iter() {
+        assert!(congruence.m != T::zero());
+        debug_assert!(gcd(n.clone(), congruence.m.clone()) == T::one());
+
+        n = n * congruence.m.clone();
+    }
+
+    let mut solution = T::zero();
+
+    for congruence in congruences.iter() {
+        let a_i = congruence.a.clone();
+        let m_i = n.clone() / congruence.m.clone();
+        let n_i = m_i.clone().invm(&congruence.m)?;
+
+        solution = (solution + a_i * (m_i % n.clone()) * n_i) % n.clone();
+    }
+
+    Some(solution)
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_chinese_remainder_theorem() {
+        let congruences = vec![
+            Congruence {
+                a: 83u64,
+                m: 101u64,
+            },
+            Congruence {
+                a: 60u64,
+                m: 103u64,
+            },
+        ];
+
+        // 2024 Day 14
+        assert_eq!(chinese_remainder_theorem(&congruences), Some(6446));
+    }
 
     #[test]
     fn test_add() {
