d4ab5d857465111a94b0ac4a1d4c6880c72af348
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-21 05:40:14 +0100

    DistanceGrid: Another convenience wrapper around Grid2D<T>

--- src/grid/mod.rs
+++ src/grid/mod.rs
@@ -1,9 +1,10 @@
+mod bool_grid;
 mod bordered_grid;
 mod coordinate;
 mod direction;
 mod direction_set;
+mod distance_grid;
 mod grid2d;
-mod bool_grid;
 mod save_to_image;
 mod transformations;
 
@@ -12,4 +13,5 @@ pub use bordered_grid::*;
 pub use coordinate::*;
 pub use direction::*;
 pub use direction_set::*;
+pub use distance_grid::*;
 pub use grid2d::*;
