fdfdd9cfc6015cc7809ad468585694d3f042e56a
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-27 09:34:37 +0100

    BoolGrid2D: Derive Hash

--- src/grid/bool_grid.rs
+++ src/grid/bool_grid.rs
@@ -8,7 +8,7 @@ use super::Grid2D;
 /// A grid of booleans
 ///
 /// This is a wrapper around Grid2D<bool> that implements Display and Debug.
-#[derive(Clone)]
+#[derive(Clone, Hash)]
 pub struct BoolGrid2D {
     grid: Grid2D<bool>,
 }
