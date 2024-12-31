1d14625a9f3b1ed53bafa3506a37915f0fa2cfd3
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-31 09:09:07 +0100

    Remove DirectionSet

--- src/grid/mod.rs
+++ src/grid/mod.rs
@@ -1,7 +1,6 @@
 mod bool_grid;
 mod coordinate;
 mod direction;
-mod direction_set;
 mod distance_grid;
 mod grid2d;
 mod save_to_image;
@@ -10,6 +9,5 @@ mod transformations;
 pub use bool_grid::*;
 pub use coordinate::*;
 pub use direction::*;
-pub use direction_set::*;
 pub use distance_grid::*;
 pub use grid2d::*;
