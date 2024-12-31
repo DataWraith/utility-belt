186ffadcf4d401df3fdc52ad5116598f3be11b38
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-31 12:34:32 +0100

    Make the Line-intersection test use Coordinates as args

--- src/math/line_line_intersection.rs
+++ src/math/line_line_intersection.rs
@@ -1,6 +1,8 @@
 use ndarray::array;
 use num::{Num, Signed};
 
+use crate::prelude::Coordinate;
+
 use super::gauss_jordan;
 
 /// This computes the intersection point (if one exists) of two lines given as pairs of points.
@@ -13,13 +15,15 @@ use super::gauss_jordan;
 /// * `a` - The first line, given as a pair of points.
 /// * `b` - The second line, given as a pair of points.
 /// * `eps` - A small value (e.g. 1e-9) to help with limited floating point precision.
-pub fn line_intersection_point<T: Num + Signed + PartialOrd + Clone + std::fmt::Debug>(
-    a: ((T, T), (T, T)),
-    b: ((T, T), (T, T)),
+pub fn line_intersection_point<T: Num + Signed + PartialOrd + PartialEq + Copy>(
+    a: (Coordinate<T>, Coordinate<T>),
+    b: (Coordinate<T>, Coordinate<T>),
     eps: T,
-) -> Option<(T, T)> {
-    let ((x1, y1), (x2, y2)) = a;
-    let ((x3, y3), (x4, y4)) = b;
+) -> Option<Coordinate<T>> {
+    let (x1, y1) = (a.0.x, a.0.y);
+    let (x2, y2) = (a.1.x, a.1.y);
+    let (x3, y3) = (b.0.x, b.0.y);
+    let (x4, y4) = (b.1.x, b.1.y);
 
     let line1_is_vertical = x1 == x2;
     let line2_is_vertical = x3 == x4;
@@ -32,24 +36,24 @@ pub fn line_intersection_point<T: Num + Signed + PartialOrd + Clone + std::fmt::
     let slope1 = if line1_is_vertical {
         T::zero()
     } else {
-        (y2.clone() - y1.clone()) / (x2.clone() - x1.clone())
+        (y2 - y1) / (x2 - x1)
     };
 
     let slope2 = if line2_is_vertical {
         T::zero()
     } else {
-        (y4.clone() - y3.clone()) / (x4.clone() - x3.clone())
+        (y4 - y3) / (x4 - x3)
     };
 
-    let intercept1 = y1.clone() - slope1.clone() * x1.clone();
-    let intercept2 = y3.clone() - slope2.clone() * x3.clone();
+    let intercept1 = y1 - slope1 * x1;
+    let intercept2 = y3 - slope2 * x3;
 
     if line1_is_vertical {
-        return Some((T::zero(), intercept2.clone()));
+        return Some((T::zero(), intercept2).into());
     }
 
     if line2_is_vertical {
-        return Some((T::zero(), intercept1.clone()));
+        return Some((T::zero(), intercept1).into());
     }
 
     if slope1 == slope2 && intercept1 == intercept2 {
@@ -72,8 +76,8 @@ pub fn line_intersection_point<T: Num + Signed + PartialOrd + Clone + std::fmt::
     //
     let matrix = array![
         //   -m * x     +   1*y    =    b
-        [-slope1.clone(), T::one(), intercept1.clone()],
-        [-slope2.clone(), T::one(), intercept2.clone()],
+        [-slope1, T::one(), intercept1],
+        [-slope2, T::one(), intercept2],
     ];
 
     let mut ans = array![T::zero(), T::zero()];
@@ -83,29 +87,41 @@ pub fn line_intersection_point<T: Num + Signed + PartialOrd + Clone + std::fmt::
         return None;
     }
 
-    Some((ans[0].clone(), ans[1].clone()))
+    let result = Coordinate::new(ans[0], ans[1]);
+
+    Some(result)
 }
 
 #[cfg(test)]
 mod tests {
-    use num::rational::Ratio;
-
     use super::*;
 
     #[test]
     fn test_line_intersection_point() {
         assert_eq!(
-            line_intersection_point(((0., 0.), (1., 1.)), ((0., 1.), (1., 0.)), 1e-9),
-            Some((0.5, 0.5))
+            line_intersection_point(
+                ((0., 0.).into(), (1., 1.).into()),
+                ((0., 1.).into(), (1., 0.).into()),
+                1e-9
+            ),
+            Some((0.5, 0.5).into())
         );
 
         assert_eq!(
-            line_intersection_point(((0.0, 0.0), (1.0, 1.0)), ((0.0, 2.0), (2.0, 0.0)), 1e-9),
-            Some((1.0, 1.0))
+            line_intersection_point(
+                ((0.0, 0.0).into(), (1.0, 1.0).into()),
+                ((0.0, 2.0).into(), (2.0, 0.0).into()),
+                1e-9
+            ),
+            Some((1.0, 1.0).into())
         );
 
         assert_eq!(
-            line_intersection_point(((0.0, 0.0), (1.0, 1.0)), ((2.0, 2.0), (3.0, 3.0)), 1e-9),
+            line_intersection_point(
+                ((0.0, 0.0).into(), (1.0, 1.0).into()),
+                ((2.0, 2.0).into(), (3.0, 3.0).into()),
+                1e-9
+            ),
             None
         )
     }
@@ -113,12 +129,20 @@ mod tests {
     #[test]
     fn test_coincidental_lines() {
         assert_eq!(
-            line_intersection_point(((0.0, 0.0), (1.0, 1.0)), ((0.0, 0.0), (1.0, 1.0)), 1e-9),
+            line_intersection_point(
+                ((0.0, 0.0).into(), (1.0, 1.0).into()),
+                ((0.0, 0.0).into(), (1.0, 1.0).into()),
+                1e-9
+            ),
             None,
         );
 
         assert_eq!(
-            line_intersection_point(((0.0, 0.0), (1.0, 1.0)), ((1.0, 1.0), (1.05, 1.05)), 1e-9),
+            line_intersection_point(
+                ((0.0, 0.0).into(), (1.0, 1.0).into()),
+                ((1.0, 1.0).into(), (1.05, 1.05).into()),
+                1e-9
+            ),
             None,
         );
     }
@@ -126,12 +150,20 @@ mod tests {
     #[test]
     fn test_parallel_lines() {
         assert_eq!(
-            line_intersection_point(((0.0, 0.0), (1.0, 1.0)), ((0.0, 1.0), (1.0, 2.0)), 1e-9),
+            line_intersection_point(
+                ((0.0, 0.0).into(), (1.0, 1.0).into()),
+                ((0.0, 1.0).into(), (1.0, 2.0).into()),
+                1e-9,
+            ),
             None,
         );
 
         assert_eq!(
-            line_intersection_point(((0.0, 0.0), (1.0, 1.0)), ((1.0, 0.0), (2.0, 1.0)), 1e-9),
+            line_intersection_point(
+                ((0.0, 0.0).into(), (1.0, 1.0).into()),
+                ((1.0, 0.0).into(), (2.0, 1.0).into()),
+                1e-9,
+            ),
             None,
         );
     }
@@ -139,40 +171,45 @@ mod tests {
     #[test]
     fn test_vertical_lines() {
         assert_eq!(
-            line_intersection_point(((0.0, 0.0), (0.0, 1.0)), ((0.0, 1.0), (1.0, 1.0)), 1e-9),
-            Some((0.0, 1.0)),
+            line_intersection_point(
+                ((0.0, 0.0).into(), (0.0, 1.0).into()),
+                ((0.0, 1.0).into(), (1.0, 1.0).into()),
+                1e-9,
+            ),
+            Some((0.0, 1.0).into()),
         );
 
         assert_eq!(
-            line_intersection_point(((0.0, 0.0), (0.0, 1.0)), ((5.0, 4.0), (-5.0, -2.0)), 1e-9),
-            Some((0.0, 1.0)),
+            line_intersection_point(
+                ((0.0, 0.0).into(), (0.0, 1.0).into()),
+                ((5.0, 4.0).into(), (-5.0, -2.0).into()),
+                1e-9,
+            ),
+            Some((0.0, 1.0).into()),
         );
 
         assert_eq!(
-            line_intersection_point(((0.0, 0.0), (0.0, 1.0)), ((1.0, 1.0), (1.0, 2.0)), 1e-9),
+            line_intersection_point(
+                ((0.0, 0.0).into(), (0.0, 1.0).into()),
+                ((1.0, 1.0).into(), (1.0, 2.0).into()),
+                1e-9,
+            ),
             None,
         );
     }
 
     #[test]
     fn test_with_rationals() {
+        use num::rational::Ratio;
+
         let x1 = Ratio::new(0xfbbb8c2bc3ed57i128, 1);
         let y1 = Ratio::new(0xb120ca41205a7ci128, 1);
+        let a = (Coordinate::new(x1, y1), Coordinate::new(x1 + 1, y1 + 1));
 
         let x2 = Ratio::new(0x649fefb18d3ed0i128, 1);
         let y2 = Ratio::new(0xfd63e6bbb2f6a9i128, 1);
+        let b = (Coordinate::new(x2, y2), Coordinate::new(x2 + 1, y2 - 1));
 
-        let dx1 = Ratio::new(1i128, 1i128);
-        let dy1 = Ratio::new(1i128, 1i128);
-
-        let dx2 = Ratio::new(1i128, 1i128);
-        let dy2 = Ratio::new(-1i128, 1i128);
-
-        assert!(line_intersection_point(
-            ((x1, y1), (x1 + dx1, y1 + dy1)),
-            ((x2, y2), (x2 + dx2, y2 + dy2)),
-            Ratio::new(0i128, 1i128)
-        )
-        .is_some());
+        assert!(line_intersection_point(a, b, Ratio::new(0i128, 1i128)).is_some());
     }
 }
