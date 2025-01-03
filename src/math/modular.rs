5a99f4cce550a9bd05d0ac02f03d6a3967517a84
Johannes Holzfuß <johannes@holzfuss.name>
2025-01-03 13:51:03 +0100

    modular: Remove unnecessary test cases

--- src/math/modular.rs
+++ src/math/modular.rs
@@ -53,34 +53,4 @@ mod tests {
         // 2024 Day 14
         assert_eq!(chinese_remainder_theorem(&congruences), Some(6446));
     }
-
-    #[test]
-    fn test_add() {
-        let (x, y, m) = (100usize, 200usize, 11usize);
-        assert_eq!(x.addm(y, &m), 3);
-    }
-
-    #[test]
-    fn test_sub() {
-        let (x, y, m) = (100usize, 200usize, 11usize);
-        assert_eq!(x.subm(y, &m), 10);
-    }
-
-    #[test]
-    fn test_mul() {
-        let (x, y, m) = (11usize, 38usize, 17usize);
-        assert_eq!(x.mulm(y, &m), 10);
-    }
-
-    #[test]
-    fn test_pow() {
-        let (x, y, m) = (2u64, 99u64, 1_000_000_007u64);
-        assert_eq!(x.powm(y, &m), 988_185_646);
-    }
-
-    #[test]
-    fn test_inv() {
-        let (x, m) = (11usize, 17usize);
-        assert_eq!(x.invm(&m), Some(14));
-    }
 }
